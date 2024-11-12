b511de41aaa39228c730fa124fb87d77d73d0211
Michael Betz <michibetz@gmail.com>
2024-11-12 02:24:32 +0100

    make double buffer optional

--- src/i2s_parallel.c
+++ src/i2s_parallel.c
@@ -218,31 +218,30 @@ void i2s_parallel_setup(i2s_dev_t *dev, const i2s_parallel_config_t *cfg) {
     dev->conf.rx_reset=0;
     dev->conf.tx_reset=0;
 
-    // This is somehow important to align all the bits of one clock cycle
-    // dev->conf.tx_right_first = 1;
-    // dev->conf.rx_right_first = 1;
-
     dev->conf1.val = 0;
     dev->conf1.tx_stop_en = 0;
     dev->timing.val = 0;
-
 
     // Allocate DMA descriptors
     i2s_state[i2snum(dev)] =
         (i2s_parallel_state_t *)malloc(sizeof(i2s_parallel_state_t));
     i2s_parallel_state_t *st = i2s_state[i2snum(dev)];
     st->desccount_a = calc_needed_dma_descs_for(cfg->bufa);
-    st->desccount_b = calc_needed_dma_descs_for(cfg->bufb);
     st->dmadesc_a = (volatile lldesc_t *)heap_caps_malloc(
         st->desccount_a * sizeof(lldesc_t), MALLOC_CAP_DMA
     );
-    st->dmadesc_b = (volatile lldesc_t *)heap_caps_malloc(
-        st->desccount_b*sizeof(lldesc_t), MALLOC_CAP_DMA
-    );
-
     // and fill them
     fill_dma_desc(st->dmadesc_a, cfg->bufa);
-    fill_dma_desc(st->dmadesc_b, cfg->bufb);
+
+    if (cfg->bufb) {
+        st->desccount_b = calc_needed_dma_descs_for(cfg->bufb);
+        st->dmadesc_b = (volatile lldesc_t *)heap_caps_malloc(
+            st->desccount_b*sizeof(lldesc_t), MALLOC_CAP_DMA
+        );
+        fill_dma_desc(st->dmadesc_b, cfg->bufb);
+    } else {
+        st->desccount_b = 0;
+    }
 
     // Reset FIFO/DMA -> needed? Doesn't dma_reset/fifo_reset do this?
     dev->lc_conf.in_rst = 1;
@@ -269,8 +268,15 @@ void i2s_parallel_setup(i2s_dev_t *dev, const i2s_parallel_config_t *cfg) {
 }
 
 void i2s_parallel_flip_to_buffer(i2s_dev_t *dev, int bufid) {
-    int no=i2snum(dev);
-    if (i2s_state[no]==NULL) return;
+    int no = i2snum(dev);
+
+    if (i2s_state[no] == NULL)
+        return;
+
+    // not using double buffering mode
+    if (i2s_state[no]->desccount_b <= 0)
+        return;
+
     lldesc_t *active_dma_chain;
     if (bufid==0) {
         active_dma_chain=(lldesc_t*)&i2s_state[no]->dmadesc_a[0];
